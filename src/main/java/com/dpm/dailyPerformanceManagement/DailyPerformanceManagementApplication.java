3c200a532569e74b681575f56f26c451d412742a
anass zeroual <anasszeroual09@gmail.com>
2024-03-16 09:09:23 +0000

    c

---
+++ src/main/java/com/dpm/dailyPerformanceManagement/DailyPerformanceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.dpm.dailyPerformanceManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyPerformanceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyPerformanceManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dpm/dailyPerformanceManagement/DailyPerformanceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpm.dailyPerformanceManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyPerformanceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
