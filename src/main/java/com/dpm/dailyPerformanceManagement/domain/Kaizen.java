a01dec40e31fc2c8c8ef465674e1dc42e37aabd3
anass zeroual <anasszeroual09@gmail.com>
2024-03-16 12:31:02 +0000

    c

---
+++ src/main/java/com/dpm/dailyPerformanceManagement/domain/Kaizen.java
@@ -0,0 +1,23 @@
+package com.dpm.dailyPerformanceManagement.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "kaizen")
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Kaizen {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double realValue;
+    private double targetValue;
+}
---
+++ src/main/java/com/dpm/dailyPerformanceManagement/domain/Quality.java
@@ -0,0 +1,22 @@
+package com.dpm.dailyPerformanceManagement.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "quality")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+public class Quality {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double realValue;
+    private double targetValue;
+}
